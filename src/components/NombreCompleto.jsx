627647d5bc3257855fbdd79dc71215ca282798a1
adriiglezs <adriiglezs0497@gmail.com>
2023-01-27 10:10:31 -0600

    inicio de archivo

---
+++ src/components/NombreCompleto.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css'
+import {Edad} from './Edad'
+
+export const NombreCompleto = (props) => {
+    console.log("props",props);
+    return (
+        <>{/* React.Frafment */}
+            <h3 className='Juan'>
+                Me llamo {props.name}
+            </h3>
+            <Edad edad={props.age} show={props.show}/>
+        </>
+    );
+};
+
+
+//export {NombreCompleto,OtraExportación}
